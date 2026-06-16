d9ea273419bc24f9004e31965d3b64cf95e62c63
archbook <Yegorim@mail.ru>
2026-06-16 15:12:53 +0300

    server initial

--- srv/main.py
+++ srv/main.py
@@ -2,50 +2,36 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
-k_sign_priv, k_sign_pub = CryptoUtils.generate_ed25519_keys()
 html = """
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Chat</title>
+        <title>LK slither.io srv</title>
     </head>
     <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://"+location.host+"/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
+        <h1> This server is for api only </h1>
     </body>
 </html>
 """
 
+now_pid = -1
+
 class World:
-    x: int = 0
-    y: int = 0
+    def __init__(self):
+        px = []
+        py = []
 
 @app.get("/")
 async def get():
     return HTMLResponse(html)
 
-@app.websocket("/wsc")
+@app.get("/pid", response_model=int)
+async def get_pid():
+    global now_pid
+    now_pid += 1
+    return now_pid
+
+@app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     try:
@@ -55,13 +41,5 @@ async def websocket_endpoint(websocket: WebSocket):
         print(await peer.recv_str())
         print(await peer.recv_str())
         print(await peer.recv_str())
-    except WebSocketDisconnect:
-        print("Client disconnected")
-
-@app.websocket("/gk")
-async def getkey_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    try:
-        await websocket.send_text(CryptoUtils.serialize_public_key(k_sign_pub))
     except WebSocketDisconnect:
         print("Client disconnected")
