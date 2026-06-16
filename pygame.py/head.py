7779164c3969a1851200b93bc9989880402a12de
Petechkaxabsolute <petechkasolomakhin@mail.ru>
2026-06-16 16:04:10 +0300

    head

--- pygame.py/head.py
+++ pygame.py/head.py
@@ -1,3 +1,14 @@
 import pygame
 pygame.init()
-screen=pygame.display.set_mode([800, 600])
+screen=pygame.display.set_mode([1000, 500])
+pygame.display.set_caption("Progect with")
+while True:
+    screen.fill([255, 255, 255])
+    events=pygame.event.get()
+    # creating themes
+    
+    for event in events:
+        if event.type==pygame.QUIT:
+            exit()
+
+    pygame.display.update()
